be943dbc9059bbfe6e09d7a7990dd81c1bdadda7
Megan Leet <megan.leet@iinet.net.au>
2017-08-27 00:48:58 +1000

    saisho no komitto

--- bansheeanalyser/cli.py
+++ bansheeanalyser/cli.py
@@ -1,10 +1,3 @@
-#!/usr/bin/env python2
-#encoding: UTF-8
-
-# To change this license header, choose License Headers in Project Properties.
-# To change this template file, choose Tools | Templates
-# and open the template in the editor.
-
 from core import *
 
 def main():
